7a765489503416a0200a893847b98079f9a05ae3
miulerbm <miulerbm00@gmail.com>
2024-05-25 22:27:56 -0500

    Adding a modal and showing the selected emoji

---
+++ components/EmojiSticker.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+const EmojiSticker = ({ imageSize, stickerSource }) => {
+  return (
+    <View style={{ top: -350 }}>
+      <Image
+        source={stickerSource}
+        resizeMode="contain"
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+};
+
+export default EmojiSticker;
