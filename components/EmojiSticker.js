eed7ed67b7cf4ab64b6fdaa6cb235702532aa94a
miulerbm <miulerbm00@gmail.com>
2024-05-27 08:43:24 -0500

    Pan gesture implemented

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -8,6 +8,16 @@ import Animated, {
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 const EmojiSticker = ({ imageSize, stickerSource }) => {
+  // new shared values: translateX and translateY
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  // new drag objecto to handle pan gesture
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
   const scaleImage = useSharedValue(imageSize);
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -23,16 +33,33 @@ const EmojiSticker = ({ imageSize, stickerSource }) => {
     };
   });
 
+  // useAnimatedStyle for returning transformations for moving the sticker
+  // this applies the transform style to the sticker element, when the right gesture triggers it
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 };
 
